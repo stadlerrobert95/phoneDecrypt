4e612a011308ea2fcae5c821bff5583594a84de5
stadlerrobert95 <driftking9508@gmail.com>
2023-02-01 16:45:58 +0100

    MappingInputToAlphabetical removed unused method and code part from convertInputNumberToAlphabetical(...) method

--- src/main/tools/generators/MappingInputToAlphabetical.java
+++ src/main/tools/generators/MappingInputToAlphabetical.java
@@ -6,14 +6,6 @@ import java.util.Map;
 
 public class MappingInputToAlphabetical {
     public static List<List<Character>> convertInputNumberToAlphabetical(Map<Character, List<Character>> phoneDial, String input) {
-        if(input.length() > 4){
-            System.out.println("The input string is longer than expected (" + input.length() + " maximum: 4)");
-            return null;
-        }
-        if(input.contains("1") || containsLetter(input)){
-            System.out.println("The input string is contains invalid character (" + input + " allowed: digits only between 2-9)");
-            return null;
-        }
         List<List<Character>> inputToCharList = new ArrayList<>();
 
         for (char c : input.toCharArray()
@@ -21,14 +13,5 @@ public class MappingInputToAlphabetical {
             inputToCharList.add(phoneDial.get(c));
         }
         return inputToCharList;
-    }
-
-    public static boolean containsLetter(String string) {
-        for (char c : string.toCharArray()) {
-            if (Character.isLetter(c)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
